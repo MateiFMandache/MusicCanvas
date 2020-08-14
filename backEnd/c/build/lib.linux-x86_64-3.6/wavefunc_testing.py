a0f76e9637447e532a9959e1bdd9d16530f9106c
Matei Mandache <matei.mandache@gmail.com>
2020-08-14 21:11:35 +0100

    wavefunc in c: added timbre functionality

--- backEnd/c/build/lib.linux-x86_64-3.6/wavefunc_testing.py
+++ backEnd/c/build/lib.linux-x86_64-3.6/wavefunc_testing.py
@@ -5,22 +5,39 @@ import wavefunc
 FRAME_RATE = 44100
 DURATION = 3
 
-timbre1 = tuple([math.sin(2 * math.pi * x) for x in range(101)])
+timbre1 = [math.sin(2 * math.pi * x/100) for x in range(101)]
+timbre2 = [1 - 2*((x/100)-math.floor(x/100)) for x in range(101)]
 note1 = {
     "start_frame": 10_000,
-    "max_amplitude": 10_000
+    "max_amplitude": 5_000,
+    "frequency": 440,
+    "timbre": {
+        "num_snapshots": 2,
+        "times": (0.3, 0.4),
+        "snapshots": (timbre2, timbre1)
+    }
 }
 note2 = {
     "start_frame": 5_000,
-    "max_amplitude": 10_000
+    "max_amplitude": 5_000,
+    "frequency": 330,
+    "timbre": {
+        "num_snapshots": 1,
+        "times": (0.0,),
+        "snapshots": (timbre1,)
+    }
 }
-note_data = (note1, note2)
+note_data = (note2,)
 
 audio = wave.open("test.wav", 'wb')
 audio.setnchannels(1)
 audio.setsampwidth(2)
 audio.setframerate(FRAME_RATE)
 audio.setnframes(DURATION * FRAME_RATE)
-byte_list = wavefunc.wavefunc(FRAME_RATE*DURATION, note_data)
-audio.writeframes(byte_list)
+byte_list1 = wavefunc.wavefunc(FRAME_RATE, note_data)
+byte_list2 = wavefunc.wavefunc(FRAME_RATE, note_data)
+byte_list3 = wavefunc.wavefunc(FRAME_RATE, note_data)
+audio.writeframesraw(byte_list1)
+audio.writeframesraw(byte_list2)
+audio.writeframesraw(byte_list3)
 audio.close()
--- backEnd/c/wavefuncmodule.c
+++ backEnd/c/wavefuncmodule.c
@@ -1,29 +1,132 @@
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
+#include <math.h>
+
+// maximum number of notes that can play at a time
+#define MAX_CONCURRENCY 16
+
+#define TIMBRE_RESOLUTION 100
+#define FRAME_RATE 44100
 
 
 typedef struct {
     int start_frame;
+    double frequency;
     short max_amplitude;
+    int num_snapshots;
+    double * timbre_times;
+    double ** timbres;
+    double time_position;
+    double timbre_position;
+    int timbre_index;
+    // width of time between previous and next mini-timbres
+    double timbre_width;
 } Note;
-
-void write_note(Note note, short result[], int frames_to_write) {
-    for (int i = note.start_frame; i < frames_to_write; i++) {
-        result[i] += note.max_amplitude;
-    }
-}
 
 void extract_data(PyObject* note_data, Note* note) {
     note->start_frame = (int) PyLong_AsLong(
         PyDict_GetItemString(note_data, "start_frame"));
     note->max_amplitude = (int) PyLong_AsLong(
         PyDict_GetItemString(note_data, "max_amplitude"));
+    note->frequency = PyFloat_AsDouble(
+        PyDict_GetItemString(note_data, "frequency"));
+
+    PyObject* timbre_info = PyDict_GetItemString(note_data, "timbre");
+
+    int num_snapshots = (int) PyLong_AsLong(
+        PyDict_GetItemString(timbre_info, "num_snapshots"));
+
+    note->num_snapshots = num_snapshots;
+
+    PyObject* py_timbre_times = PyDict_GetItemString(timbre_info, "times");
+    note->timbre_times = malloc(num_snapshots * sizeof(double));
+    for (int i = 0; i < num_snapshots; i++) {
+        note->timbre_times[i] = PyFloat_AsDouble(
+            PyTuple_GetItem(py_timbre_times, (Py_ssize_t) i));
+    }
+    PyObject* py_snapshots = PyDict_GetItemString(timbre_info, "snapshots");
+    note->timbres = malloc(num_snapshots * sizeof(double *));
+    for (int i = 0; i < num_snapshots; i++) {
+        note->timbres[i] = malloc((TIMBRE_RESOLUTION + 1) * sizeof(double));
+        PyObject * py_snapshot = PyTuple_GetItem(py_snapshots, (Py_ssize_t) i);
+        for (int j = 0; j < TIMBRE_RESOLUTION + 1; j++) {
+            note->timbres[i][j] = PyFloat_AsDouble(
+                PyList_GetItem(py_snapshot, (Py_ssize_t) j));
+        }
+    }
+
+    note->time_position = 0.0;
+    note->timbre_position = 0.0;
+    note->timbre_index = 0;
+    note->timbre_width = 0.0;
 }
 
 int write_notewave(Note * note, int frames_to_write, short wave[]) {
-    for (int index = note->start_frame; index < frames_to_write; index++) {
-        wave[index] += note->max_amplitude;
+    // Writes the wavefunction given by note to the wavefunction wave.
+    // The wavefunctions get superimposed.
+    // Returns true if the note finishes, false if it still needs to
+    // play.
+    for (int i = note->start_frame; i < frames_to_write; i++) {
+        double amplitude = (double) note->max_amplitude;
+
+        double time = note->time_position;
+
+        int snapshots = note->num_snapshots;
+        double pos = note->timbre_position;
+        // We linearly interpolate between two points in the timbre
+        // profile.
+        int timbre_point_one = (int) floor(pos);
+        int timbre_point_two = timbre_point_one + 1;
+        int timbre_coefficient_two = pos-timbre_point_one;
+        int timbre_coefficient_one = 1-timbre_coefficient_two;
+        int index = note->timbre_index;
+        if (index == 0) {
+            // before first timbre control point
+            amplitude *= (note->timbres[0][timbre_point_one]
+                            * timbre_coefficient_one
+                          + note->timbres[0][timbre_point_two]
+                            * timbre_coefficient_two);
+        } else if (index == snapshots) {
+            // after last timbre control point
+            amplitude *= (note->timbres[snapshots - 1][timbre_point_one]
+                            * timbre_coefficient_one
+                          + note->timbres[snapshots - 1][timbre_point_two]
+                            * timbre_coefficient_two);
+        } else {
+            // between two timbre control points. Linearly interpolate
+            double snapshot_coefficient_two = (time - note->timbre_times[index-1])
+                                              /note->timbre_width;
+            double snapshot_coefficient_one = 1 - snapshot_coefficient_two;
+            amplitude *= ((note->timbres[index - 1][timbre_point_one]
+                             * timbre_coefficient_one
+                           + note->timbres[index - 1][timbre_point_two]
+                             * timbre_coefficient_two) *
+                             snapshot_coefficient_one
+                          + (note->timbres[index][timbre_point_one]
+                             * timbre_coefficient_one
+                           + note->timbres[index][timbre_point_two]
+                             * timbre_coefficient_two) *
+                             snapshot_coefficient_two);
+        }
+
+        wave[i] += (short)amplitude;
+
+        // update position variables
+
+        note->timbre_position += ((note->frequency) * TIMBRE_RESOLUTION / FRAME_RATE);
+        if (note->timbre_position >= 100.0)
+            note->timbre_position -= 100.0;
+        note->time_position += 1.0 / FRAME_RATE;
+        while (note->timbre_index < snapshots
+               && note->time_position > note->timbre_times[note->timbre_index]) {
+            note->timbre_index++;
+            note->timbre_width = note->timbre_times[note->timbre_index]
+                                 - note->timbre_times[note->timbre_index - 1];
+        }
     }
+    // if this note continues to be played,
+    // set start_frame to 0 and return false.
+    note->start_frame = 0;
     return 0;
 }
 
@@ -35,27 +138,63 @@ wavefunc(PyObject* self, PyObject* args)
 
     if (!PyArg_ParseTuple(args, "iO", &frames_to_write, &notes_data))
         return NULL;
-
-    short output_nums[frames_to_write];
+    // initialise wave function
+    short wave[frames_to_write];
 
     for (int i = 0; i < frames_to_write; i++) {
-        output_nums[i] = 0;
+        wave[i] = 0;
     }
+
+    static Note * active_notes[MAX_CONCURRENCY];
+    // flag representing if this is the start of the wavefunction
+    static int first_run = 1;
+    if (first_run) {
+        for (int i = 0; i < MAX_CONCURRENCY; i++) {
+            active_notes[i] = NULL;
+        }
+    }
+
+    Note * still_playing_notes[MAX_CONCURRENCY];
+    for (int i = 0; i < MAX_CONCURRENCY; i++) {
+        still_playing_notes[i] = NULL;
+    }
+    int still_playing_index = 0;
 
     int number_of_notes = (int) PyTuple_Size(notes_data);
 
     Note * notes[number_of_notes];
 
+    // add on wavefunction for notes that are still playing
+    for (int i = 0; i < MAX_CONCURRENCY; i++) {
+        if (active_notes[i] != NULL) {
+            if (write_notewave(active_notes[i], frames_to_write, wave)) {
+                free(notes[i]);
+            } else {
+                still_playing_notes[still_playing_index] = notes[i];
+                still_playing_index++;
+            }
+        }
+    }
+
+    // add on wavefunction for new notes
     for (int i = 0; i < number_of_notes; i++) {
         notes[i] = (Note * ) malloc(sizeof(Note));
         extract_data(PyTuple_GetItem(notes_data, i), notes[i]);
-        if (write_notewave(notes[i], frames_to_write, output_nums))
+        if (write_notewave(notes[i], frames_to_write, wave)) {
             free(notes[i]);
-        else
-            printf("remaining note");
+        } else {
+            still_playing_notes[still_playing_index] = notes[i];
+            still_playing_index++;
+        }
     }
 
-    char * output = (char *) &output_nums;
+    for (int i = 0; i < MAX_CONCURRENCY; i++) {
+        active_notes[i] = still_playing_notes[i];
+    }
+
+    first_run = 0;
+
+    char * output = (char *) &wave;
 
     return PyBytes_FromStringAndSize(output, 2*frames_to_write);
 }
